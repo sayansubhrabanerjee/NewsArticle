1ca12f67f582ba68fdb388f900bba6c5b67691f3
h260210 <sayan.banerjee@honeywell.com>
2019-04-27 13:41:29 +0530

    project setup

--- app/src/main/java/sayan/banerjee/newsarticle/ArticleActivity.kt
+++ app/src/main/java/sayan/banerjee/newsarticle/ArticleActivity.kt
@@ -3,7 +3,7 @@ package sayan.banerjee.newsarticle
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ArticleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
